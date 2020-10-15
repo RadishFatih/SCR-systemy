7b42a8e5cebe87bc17e89775301a2c5994e24e07
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2020-10-15 19:07:35 +0200

    Update cw3.sh

--- cw3.sh
+++ cw3.sh
@@ -2,7 +2,6 @@
 
 for found_file in $(find . -name "*$1" -print);
 do
-    # Gets the number of days since the last modification of the file
     mod=$(date -r $found_file +%s)
     now=$(date +%s)
     days=$(expr \( $now - $mod \) / 86400)
